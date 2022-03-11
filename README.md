64e89a75852295f5710b52f11d040b075e01ff6a
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-03-11 21:58:54 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# What's this then?
+
+This is my solution for the first homework assignment of a software development course.
---
+++ library.py
@@ -0,0 +1,58 @@
+library = {
+    "name": "CodeClan Library",
+    "books": [
+        {
+            "author": "George RR Martin",
+            "title": "A Song of Ice and Fire"
+        },
+        {
+            "author": "J R. R. Tolkien",
+            "title": "The Hobbit"
+        },
+        {
+            "author": "Paul Barry",
+            "title": "Head-First Python"
+        },
+        {
+            "author": "Allen B. Downey",
+            "title": "Think Python: How to Think Like a Computer Scientist"
+        },
+        {
+            "author": "Eric Matthes",
+            "title": "Python Crash Course"
+        }
+    ]
+}
+
+# TODO - Print welcome statement including library name
+
+option = ""
+while option != "q":
+    print("Options:")
+    print("1 - List all books")
+    print("2 - Search for a book by title")
+    print("3 - Add a book")
+    print("4 - Remove a book")
+    print("5 - Update a book")
+    print("q - Quit")
+    option = input("What would you like to do? \n")
+
+    if option == "1":
+        print("Listing all books...")
+        # TODO - List all books
+
+    if option == "2":
+        print("Searching for a book by title...")
+        # TODO - Search for a book by title
+
+    if option == "3":
+        print("Adding a book...")
+        # TODO - Add a book
+
+    if option == "4":
+        print("Removing a book...")
+        # TODO - Remove a book
+
+    if option == "5":
+        print("Updating a book...")
+        # TODO - Update a book
